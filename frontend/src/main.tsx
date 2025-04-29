29029fa610a2c05bba38611ce92c5e7eb0b8fa9d
Roy Kincaid <roy@kincaid.ninja>
2025-04-28 19:09:36 -0700

    added amplify_outputs.json ton main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Amplify } from "aws-amplify";
+import outputs from "../../amplify_outputs.json";
+
+Amplify.configure(outputs);
+const existingConfig = Amplify.getConfig();
+Amplify.configure({
+  ...existingConfig,
+  API: {
+    ...existingConfig.API,
+    REST: outputs.custom.API,
+  },
+});
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
